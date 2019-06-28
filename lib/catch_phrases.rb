818c740245b5a0f949eb4c23412ba7264637a2f7
Jason Viola <jason.viola@mavs.uta.edu>
2019-06-28 02:31:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,4 +7,13 @@ end
 def toadstool
   puts $status
 end
+
 def link
+  zelda_quote = "It's Dangerous To Go Alone! Take This!"
+puts zelda_quote
+end
+
+def any_phrase
+  phrase = "Do A Barrell Roll!"
+  puts phrase
+end
